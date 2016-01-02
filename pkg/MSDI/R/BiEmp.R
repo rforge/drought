e4d115b5a90491543478f759b36d801800fc0dc6
zhao2014 <zhao2014@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-02 15:31:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/drought@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/MSDI/R/BiEmp.R
@@ -0,0 +1,33 @@
+#' Compute the empirical joint probability
+#' @param X The vector of a monthly hydro-climatic variable of n years.
+#' @param Y The vector of a monthly hydro-climatic variable of n years.  
+#' @return The empirical joint probability time scale 
+#' @export
+#' @examples
+#' X=runif(120, min = 0, max = 100)
+#' Y=runif(120, min = 0, max = 100)
+#' fit<-BiEmp(X,Y) 
+
+
+BiEmp<-function (X,Y)
+  
+{
+X=matrix(X,ncol=1)
+Y=matrix(Y,ncol=1)
+
+n=length(X)
+
+Z=matrix(NA,nrow=length(X),ncol=1)
+ 
+for  (k in 1:n)
+{  
+  Z[k]=sum((X<=X[k])&(Y<=Y[k])) 
+  # Weibull
+  Z[k]=Z[k]/(n+1)
+  #  Gringorten   Z[k]=(Z[k]-0.44)/(n+0.12)
+  
+} 
+
+return(Z)
+
+}
